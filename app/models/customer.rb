6c59c041a4912db169db4c2c401c6188d2776953
Federica Macchi <federica.macchi3@gmail.com>
2019-06-11 11:57:37 +0100

    commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,32 @@
 class Customer
   attr_reader :first_name, :last_name
-
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, content, rating)
+    new_review = Review.new(self, restaurant, rating, content)
+  end
+
+  def reviews
+    Review.all.select {|review| review.customer == self}
+  end
+
+  def num_reviews
+    reviews.count
+  end
+
+  
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,35 @@
 class Review
+
+    attr_reader :customer, :restaurant, :content
+    attr_accessor :rating
+    @@all = []
+
+    def initialize(customer, restaurant, rating, content)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @content = content
+        @@all << self
+    end
   
+    def self.all
+        @@all
+    end
+
+    def rating=(num)
+        num = num.to_i
+        # binding.pry
+        if num > 5
+            @rating = 5
+        elsif num < 1
+            @rating = 1
+        end
+        return @rating
+        binding.pry
+
+    end
+
+
+
 end
 
